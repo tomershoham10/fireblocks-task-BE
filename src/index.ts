e55b455a09ec2b2f8c6a770717ed37223ae3c0a8
tomershoham10 <tomershoham10@gmail.com>
2025-03-09 20:31:09 +0200

    feat: validate environment variables and refactor config exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import startServer from './server.js';
+import { ADRESS, PRIVATE_KEY, RPC_URL } from './utils/config.js';
 
 (async () => {
   try {
+    console.log('Validating environment variables...');
+    // Check if all required environment variables are set
+  if (!RPC_URL || !PRIVATE_KEY || !ADRESS) {
+    throw new Error('Missing environment variables');
+  }
     console.log('server starting...');
     startServer();
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 })();
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const config = {
-  http: {
-    port: process.env.PORT || 8080,
-  },
-  db: {},
-};
 
-export default config;
+export const RPC_URL = process.env.RPC_URL ?? '';
+export const PRIVATE_KEY = process.env.PRIVATE_KEY ?? '';
+export const ADRESS = process.env.CONTRACT_ADDRESS ?? '';
+export const PORT = process.env.PORT || 8080;
